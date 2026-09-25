6570cab0217a09754265b663b8302ff5ec704dd1
Hubert Trebicki <hubertrebicki@gmail.com>
2026-09-25 17:09:14 +0200

    feat: add progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <print>
 
 int main() {
@@ -9,6 +10,7 @@ int main() {
     std::println("255");
 
     for (size_t j = 0; j < image_height; j++) {
+        std::print(std::clog, "\rScanlines remaining: {:<5}", image_height - j);
         for (size_t i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -21,4 +23,5 @@ int main() {
             std::println("{} {} {}", ir, ig, ib);
         }
     }
+    std::println(std::clog, "\rDone.                   ");
 }
